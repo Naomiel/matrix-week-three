d5d057807ffb4effd342f25c5305599099c90d7c
Naomiel <naomieloor@gmail.com>
2023-06-30 19:01:23 -0700

    completed task-three

--- src/email_parser.py
+++ src/email_parser.py
@@ -3,13 +3,24 @@ import re
 
 class EmailParser:
     # regex pattern that validate email
-    pattern = r''
+    pattern = r'^([a-zA-Z][a-zA-Z0-9]*)@([a-zA-Z][a-zA-Z0-9]*\.com)$'
+
     keys = ['username', 'domain']
 
     def parse(self, email):
         ''' parse email and return a dictionary of username and domain if valid and return None otherwise'''
-        # Add implementation here ...
+        match = re.match(self.regex_pattern, email)
 
-    def convert_to_dict(self):
+        if match:
+            username = match.group(1)
+            domain = match.group(2)
+
+            return {'username': username, 'domain': domain}
+        
+        return None
+
+
+    def convert_to_dict(self, keys, values):
         '''
           Takes in two lists as params and return them as a dictionary.'''
+        return dict(zip(keys, values))
--- tests/test_email_parser.py
+++ tests/test_email_parser.py
@@ -1,3 +1,28 @@
 # Import all required modules
 
 from unittest import TestCase
+
+
+class EmailParser(TestCase):
+    def setUp(self):
+        self.email_parser = EmailParser()
+
+    def test_parse_valid_email(self):
+        email = 'johndoe@gmail.com'
+        expected_output = {'username': 'johndoe', 'domain': 'gmail.com'}
+        parsed_email = self.email_parser.parse(email)
+        self.assertEqual(parsed_email, expected_output)
+
+    def test_parse_invalid_email(self):
+        email = 'johndoe@invalid'
+        parsed_email = self.email_parser.parse(email)
+        self.assertIsNone(parsed_email)
+
+    def test_convert_to_dict(self):
+        values = ['johndoe', 'gmail.com']
+        expected_output = {'username': 'johndoe', 'domain': 'gmail.com'}
+        converted_dict = self.email_parser.convert_to_dict(values, self.email_parser.keys)
+        self.assertEqual(converted_dict, expected_output)
+
+if __name__ == '__main__':
+    TestCase.main()
